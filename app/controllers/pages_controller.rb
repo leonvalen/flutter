13fd5634cd76e2c22921a8d055dd853251b5f0ec
Jose Leonardo Valenzuela <leonvalen@gmail.com>
2017-04-20 22:26:04 -0300

    Primera actualización, todos los archivos del proyecto

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,22 @@
+# el Pages_controller.rb contiene todo el código para cualquier página 
+# dentro de /Pages
+
+class PagesController < ApplicationController
+  
+# Backend para pages/index
+  def index
+  end
+
+# Backend para pages/home
+  def home
+  end
+
+# Backend para pages/profile
+  def profile
+  end
+
+# Backend para pages/explore
+  def explore
+  end
+  
+end
