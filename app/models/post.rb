b3cc44b6fe8eec8b4d3021e6812fc52a600213e8
Jose Leonardo Valenzuela <leonvalen@gmail.com>
2017-04-24 00:56:05 -0300

    Creacion de la tabla de posts con sus campos, cuantos post tiene cada usuario, y ordenarlos por fecha los nuevos tweets primero

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+  belongs_to :user
+  validates :user_id, presence: true #validar que esté el usuario
+  validates :content, presence: true, length: { maximum: 140 } # los twits son hasta 140 carácteres
+  default_scope -> { order(created_at: desc) } # tweets con nuevos post primero
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170423043009) do
+ActiveRecord::Schema.define(version: 20170424031049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "adminpack"
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at", using: :btree
+    t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -35,4 +44,5 @@ ActiveRecord::Schema.define(version: 20170423043009) do
     t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
   end
 
+  add_foreign_key "posts", "users"
 end
